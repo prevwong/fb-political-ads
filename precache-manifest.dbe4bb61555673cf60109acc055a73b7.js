e41cd904a0a6801a4c4274dca82749c7429cc39d
Prev Wong <prevwong@gmail.com>
2020-03-11 03:30:20 +0800

    Updates

---
+++ precache-manifest.dbe4bb61555673cf60109acc055a73b7.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "38f7af0ff71e682eb9afa9e012b754ec",
+    "url": "/fb-political-ads/index.html"
+  },
+  {
+    "revision": "d5a7408d9bd320c8d67b",
+    "url": "/fb-political-ads/static/css/2.56bbed46.chunk.css"
+  },
+  {
+    "revision": "81c28efb646a02962077",
+    "url": "/fb-political-ads/static/css/main.038f65b0.chunk.css"
+  },
+  {
+    "revision": "d5a7408d9bd320c8d67b",
+    "url": "/fb-political-ads/static/js/2.c0d11578.chunk.js"
+  },
+  {
+    "revision": "df8be702f0cc9d299c0ff42ca03d3e40",
+    "url": "/fb-political-ads/static/js/2.c0d11578.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "81c28efb646a02962077",
+    "url": "/fb-political-ads/static/js/main.35c696db.chunk.js"
+  },
+  {
+    "revision": "9f6da674d9dafadce0d5",
+    "url": "/fb-political-ads/static/js/runtime-main.d643e269.js"
+  },
+  {
+    "revision": "b02bdc1b846fd65473922f5f62832108",
+    "url": "/fb-political-ads/static/media/element-icons.b02bdc1b.ttf"
+  },
+  {
+    "revision": "d2f69a92faa6fe990d2e613c358be705",
+    "url": "/fb-political-ads/static/media/element-icons.d2f69a92.woff"
+  }
+]);
